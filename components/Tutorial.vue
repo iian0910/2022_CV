f818c4dd5fd59c000d13936d3c0912300f857a62
Ian Fan <ian@pollex.com.tw>
2022-11-25 17:03:45 +0800

    first commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,27 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <div class="section">
+      <div class="box"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.section {
+  width: 100%;
+  height: 3550px;
+  background-color: rgba(0,0,0,0.1);
+}
+.box {
+  width: 300px;
+  height: 300px;
+  background-color: gray;
+}
+</style>
+
+<script>
+export default {
+  name: 'NuxtTutorial'
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,62 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'CV',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    'nuxt-gsap-module',
+    '@nuxtjs/eslint-module',
+  ],
+
+  gsap: {
+    extraPlugins: {
+      scrollTo: true,
+      scrollTrigger: true
+    },
+    extraEases: {
+      expoScaleEase: true
+    }
+  },
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
